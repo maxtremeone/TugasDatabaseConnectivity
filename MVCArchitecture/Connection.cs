0a972c2fe11f9248513bd60622ea082bbff595cb
unknown <ahlulhalliwal09@gmail.com>
2023-07-13 22:26:16 +0700

    Update Connection.cs

--- MVCArchitecture/Connection.cs
+++ MVCArchitecture/Connection.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 
-namespace MVCArchitecture
+namespace DatabaseConnectivity;
+
+public class Connection
 {
-    internal class Class1
+    private static string _connectionString = "Data Source=CAMOUFLY;Database=db_mcc_79;Integrated Security=True;Connect Timeout=30;";
+
+    private static SqlConnection _connection;
+
+    public static SqlConnection Get()
     {
+        try
+        {
+            _connection = new SqlConnection(_connectionString);
+            return _connection;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
