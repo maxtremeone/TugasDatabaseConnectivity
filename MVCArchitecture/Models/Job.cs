a8a4739330b25b19b3f1dd821a8da248e9fbd85f
unknown <ahlulhalliwal09@gmail.com>
2023-07-13 15:40:02 +0700

    Create Job.cs

---
+++ MVCArchitecture/Models/Job.cs
@@ -0,0 +1,238 @@
+﻿using MVCArchitecture;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DatabaseConnectivity.Models;
+public class Job
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public int MinSalary { get; set; }
+    public int MaxSalary { get; set; }
+
+    public List<Job> GetAll()
+    {
+        var connection = Connection.Get();
+
+        var jobs = new List<Job>();
+
+        using SqlCommand sqlCommand = new SqlCommand();
+        sqlCommand.Connection = connection;
+        sqlCommand.CommandText = "SELECT * FROM jobs";
+
+        try
+        {
+            connection.Open();
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Job job = new Job();
+                    job.Id = reader.GetString(0);
+                    job.Title = reader.GetString(1);
+                    job.MinSalary = reader.GetInt32(2);
+                    job.MaxSalary = reader.GetInt32(3);
+
+                    jobs.Add(job);
+                }
+            }
+            else
+            {
+                reader.Close();
+                connection.Close();
+            }
+
+            return jobs;
+        }
+        catch
+        {
+            return new List<Job>();
+        }
+    }
+
+
+    public int Insert(Job job)
+    {
+        var connection = Connection.Get();
+
+        using SqlCommand sqlCommand = new SqlCommand();
+        sqlCommand.Connection = connection;
+        sqlCommand.CommandText = "INSERT INTO jobs VALUES (@id, @title, @min_salary, @max_salary)";
+
+        connection.Open();
+        using SqlTransaction transaction = connection.BeginTransaction();
+        sqlCommand.Transaction = transaction;
+
+        try
+        {
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = SqlDbType.VarChar;
+            pId.Value = job.Id;
+            sqlCommand.Parameters.Add(pId);
+
+            SqlParameter pTitle = new SqlParameter();
+            pTitle.ParameterName = "@title";
+            pTitle.SqlDbType = SqlDbType.VarChar;
+            pTitle.Value = job.Title;
+            sqlCommand.Parameters.Add(pTitle);
+
+            SqlParameter pMinSalary = new SqlParameter();
+            pMinSalary.ParameterName = "@min_salary";
+            pMinSalary.SqlDbType = SqlDbType.Int;
+            pMinSalary.Value = job.MinSalary;
+            sqlCommand.Parameters.Add(pMinSalary);
+
+            SqlParameter pMaxSalary = new SqlParameter();
+            pMaxSalary.ParameterName = "@max_salary";
+            pMaxSalary.SqlDbType = SqlDbType.Int;
+            pMaxSalary.Value = job.MaxSalary;
+            sqlCommand.Parameters.Add(pMaxSalary);
+
+            int result = sqlCommand.ExecuteNonQuery();
+
+            transaction.Commit();
+            connection.Close();
+
+            return result; // 0 gagal, >= 1 berhasil
+        }
+        catch
+        {
+            transaction.Rollback();
+            return -1; // Kesalahan terjadi pada database
+        }
+    }
+
+
+    public int Update(Job job)
+    {
+        var connection = Connection.Get();
+
+        using SqlCommand sqlCommand = new SqlCommand();
+        sqlCommand.Connection = connection;
+        sqlCommand.CommandText = "UPDATE jobs SET title = @title, min_salary = @min_salary, max_salary = @max_salary WHERE id = @id";
+
+        connection.Open();
+        using SqlTransaction transaction = connection.BeginTransaction();
+        sqlCommand.Transaction = transaction;
+
+        try
+        {
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = SqlDbType.VarChar;
+            pId.Value = job.Id;
+            sqlCommand.Parameters.Add(pId);
+
+            SqlParameter pTitle = new SqlParameter();
+            pTitle.ParameterName = "@title";
+            pTitle.SqlDbType = SqlDbType.VarChar;
+            pTitle.Value = job.Title;
+            sqlCommand.Parameters.Add(pTitle);
+
+            SqlParameter pMinSalary = new SqlParameter();
+            pMinSalary.ParameterName = "@min_salary";
+            pMinSalary.SqlDbType = SqlDbType.Int;
+            pMinSalary.Value = job.MinSalary;
+            sqlCommand.Parameters.Add(pMinSalary);
+
+            SqlParameter pMaxSalary = new SqlParameter();
+            pMaxSalary.ParameterName = "@max_salary";
+            pMaxSalary.SqlDbType = SqlDbType.Int;
+            pMaxSalary.Value = job.MaxSalary;
+            sqlCommand.Parameters.Add(pMaxSalary);
+
+            int result = sqlCommand.ExecuteNonQuery();
+
+            transaction.Commit();
+            connection.Close();
+
+            return result;
+        }
+        catch
+        {
+            transaction.Rollback();
+            return -1;
+        }
+    }
+
+
+    public int Delete(int id)
+    {
+        var connection = Connection.Get();
+
+        using SqlCommand sqlCommand = new SqlCommand();
+        sqlCommand.Connection = connection;
+        sqlCommand.CommandText = "DELETE FROM jobs WHERE id = @id";
+
+        connection.Open();
+        using SqlTransaction transaction = connection.BeginTransaction();
+        sqlCommand.Transaction = transaction;
+
+        try
+        {
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = SqlDbType.VarChar;
+            pId.Value = id;
+            sqlCommand.Parameters.Add(pId);
+
+            int result = sqlCommand.ExecuteNonQuery();
+
+            transaction.Commit();
+            connection.Close();
+
+            return result;
+        }
+        catch
+        {
+            transaction.Rollback();
+            return -1;
+        }
+    }
+
+
+    public Job GetById(int id)
+    {
+        var job = new Job();
+
+        var connection = Connection.Get();
+
+        SqlCommand sqlCommand = new SqlCommand();
+        sqlCommand.Connection = connection;
+        sqlCommand.CommandText = "SELECT * FROM jobs WHERE Id = @id";
+
+        SqlParameter pId = new SqlParameter();
+        pId.ParameterName = "@id";
+        pId.SqlDbType = SqlDbType.VarChar;
+        pId.Value = id;
+        sqlCommand.Parameters.Add(pId);
+
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+
+                job.Id = reader.GetString(0);
+                job.Title = reader.GetString(1);
+                job.MinSalary = reader.GetInt32(2);
+                job.MaxSalary = reader.GetInt32(3);
+            }
+
+            reader.Close();
+            connection.Close();
+
+            return new Region();
+        }
+        catch
+        {
+            return new Region();
+        }
+    }
+}
