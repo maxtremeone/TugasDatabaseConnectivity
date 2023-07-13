24451d23dc2cc154b22bd50c1ff592940c767f3a
unknown <ahlulhalliwal09@gmail.com>
2023-07-13 22:25:28 +0700

    Update VRegion.cs

--- MVCArchitecture/Views/VRegion.cs
+++ MVCArchitecture/Views/VRegion.cs
@@ -1,82 +1,97 @@
-﻿using DatabaseConnectivity.Models;
+﻿using System;
+using System.Collections.Generic;
+using DatabaseConnectivity.Models;
 
-namespace DatabaseConnectivity.Views;
-
-public class VRegion
+namespace DatabaseConnectivity.Views
 {
-    public void GetAll(List<Region> regions)
+    public class VRegion
     {
-        foreach (var region in regions)
+        public void GetAll(List<Region> regions)
         {
-            GetById(region);
+            foreach (var region in regions)
+            {
+                GetById(region);
+            }
         }
-    }
 
-    public void GetById(Region region)
-    {
-        Console.WriteLine("Id: " + region.Id);
-        Console.WriteLine("Name: " + region.Name);
-        Console.WriteLine("==========================");
-    }
-
-    public void DataEmpty()
-    {
-        Console.WriteLine("Data Not Found!");
-    }
-
-    public void Success()
-    {
-        Console.WriteLine("Success!");
-    }
-
-    public void Failure()
-    {
-        Console.WriteLine("Fail, Id not found!");
-    }
-
-    public void Error()
-    {
-        Console.WriteLine("Error retrieving from database!");
-    }
-
-    public int Menu()
-    {
-        Console.WriteLine("== Menu Region ==");
-        Console.WriteLine("1. Tambah");
-        Console.WriteLine("2. Update");
-        Console.WriteLine("3. Hapus");
-        Console.WriteLine("4. Search By Id");
-        Console.WriteLine("5. Get All");
-        Console.WriteLine("6. Main Menu");
-        Console.WriteLine("Pilih: ");
-
-        int input = Int32.Parse(Console.ReadLine());
-        return input;
-    }
-
-    public Region InsertMenu()
-    {
-        Console.WriteLine("Masukan Nama: ");
-        string? inputName = Console.ReadLine();
-
-        return new Region
+        public void GetById(Region region)
         {
-            Id = 0,
-            Name = inputName
-        };
-    }
+            Console.WriteLine("Id: " + region.Id);
+            Console.WriteLine("Name: " + region.Name);
+            Console.WriteLine("==========================");
+        }
 
-    public Region UpdateMenu()
-    {
-        Console.WriteLine("Id yang ingin diubah: ");
-        int id = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Nama: ");
-        string name = Console.ReadLine();
-
-        return new Region
+        public void DataEmpty()
         {
-            Id = id,
-            Name = name
-        };
+            Console.WriteLine("Data Not Found!");
+        }
+
+        public void Success()
+        {
+            Console.WriteLine("Success!");
+        }
+
+        public void Failure()
+        {
+            Console.WriteLine("Fail, Id not found!");
+        }
+
+        public void Error()
+        {
+            Console.WriteLine("Error retrieving from the database!");
+        }
+
+        public int Menu()
+        {
+            Console.WriteLine("== Region Menu ==");
+            Console.WriteLine("1. Insert Region");
+            Console.WriteLine("2. Update Region");
+            Console.WriteLine("3. Delete Region");
+            Console.WriteLine("4. Search Region by ID");
+            Console.WriteLine("5. Get All Regions");
+            Console.WriteLine("6. Main Menu");
+            Console.WriteLine("Select an option: ");
+
+            int input = Int32.Parse(Console.ReadLine());
+            return input;
+        }
+
+        public Region InsertMenu()
+        {
+            Console.WriteLine("Enter Name: ");
+            string inputName = Console.ReadLine();
+
+            return new Region
+            {
+                Id = 0,
+                Name = inputName
+            };
+        }
+
+        public Region UpdateMenu()
+        {
+            Console.WriteLine("Enter the ID to update: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter new Name: ");
+            string name = Console.ReadLine();
+
+            return new Region
+            {
+                Id = id,
+                Name = name
+            };
+        }
+
+        public int DeleteMenu()
+        {
+            Console.WriteLine("Enter the ID to delete: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            return id;
+        }
+
+        internal int SearchByIdMenu()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
