de30f5458ee5a11c94f08da35ad8a4941f62532f
unknown <ahlulhalliwal09@gmail.com>
2023-07-13 15:39:53 +0700

    Create RegionController.cs

---
+++ MVCArchitecture/Controllers/RegionController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCArchitecture.Controller
+{
+    internal class Class1
+    {
+    }
+}
