2549757545aa08ec1e859612f058e9458ebe654a
unknown <ahlulhalliwal09@gmail.com>
2023-07-13 22:26:03 +0700

    Update RegionController.cs

--- MVCArchitecture/Controllers/RegionController.cs
+++ MVCArchitecture/Controllers/RegionController.cs
@@ -1,12 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DatabaseConnectivity.Models;
+using DatabaseConnectivity.Views;
+using System;
 
-namespace MVCArchitecture.Controller
+namespace DatabaseConnectivity.Controllers
 {
-    internal class Class1
+    public class RegionController
     {
+        private Region _regionModel;
+        private VRegion _regionView;
+
+        public RegionController(Region regionModel, VRegion regionView)
+        {
+            _regionModel = regionModel;
+            _regionView = regionView;
+        }
+
+        public void GetAllRegion()
+        {
+            var result = _regionModel.GetAll();
+            if (result.Count == 0)
+            {
+                _regionView.DataEmpty();
+            }
+            else
+            {
+                _regionView.GetAll(result);
+            }
+        }
+
+        public void InsertRegion()
+        {
+            var region = _regionView.InsertMenu();
+            int result = _regionModel.Insert(region);
+            switch (result)
+            {
+                case -1:
+                    _regionView.Error();
+                    break;
+                case 0:
+                    _regionView.Failure();
+                    break;
+                default:
+                    _regionView.Success();
+                    break;
+            } 
+        }
+
+        public void UpdateRegion()
+        {
+            var region = _regionView.UpdateMenu();
+            int result = _regionModel.Update(region);
+
+            switch (result)
+            {
+                case -1:
+                    _regionView.Error();
+                    break;
+                case 0:
+                    _regionView.Failure();
+                    break;
+                default:
+                    _regionView.Success();
+                    break;
+            }
+        }
+
+        public void DeleteRegion()
+        {
+            int id = _regionView.DeleteMenu();
+            int result = _regionModel.Delete(id);
+
+            switch (result)
+            {
+                case -1:
+                    _regionView.Error();
+                    break;
+                case 0:
+                    _regionView.Failure();
+                    break;
+                default:
+                    _regionView.Success();
+                    break;
+            }
+        }
+
+        public void SearchRegionById()
+        {
+            int id = _regionView.SearchByIdMenu();
+            var region = _regionModel.GetById(id);
+            
+            if (region == null)
+            {
+                _regionView.Failure();
+            }
+            else
+            {
+                _regionView.GetById(region);
+            }
+        }
     }
 }
