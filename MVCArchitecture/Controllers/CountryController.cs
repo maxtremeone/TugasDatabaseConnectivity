fe7b730f59afe91921c4ecab24c58ee7d3925a0a
unknown <ahlulhalliwal09@gmail.com>
2023-07-14 00:28:03 +0700

    Update CountryController.cs

--- MVCArchitecture/Controllers/CountryController.cs
+++ MVCArchitecture/Controllers/CountryController.cs
@@ -1,102 +1,102 @@
-﻿//using DatabaseConnectivity.Models;
-//using DatabaseConnectivity.Views;
-//using System;
+﻿using DatabaseConnectivity.Models;
+using DatabaseConnectivity.Views;
+using System;
 
-//namespace DatabaseConnectivity.Controllers
-//{
-//    public class CountryController
-//    {
-//        private Country _countryModel;
-//        private VCountry _countryView;
+namespace DatabaseConnectivity.Controllers
+{
+    public class CountryController
+    {
+        private Country _countryModel;
+        private VCountry _countryView;
 
-//        public CountryController(Country countryModel, VCountry countryView)
-//        {
-//            _countryModel = countryModel;
-//            _countryView = countryView;
-//        }
+        public CountryController(Country countryModel, VCountry countryView)
+        {
+            _countryModel = countryModel;
+            _countryView = countryView;
+        }
 
-//        public void GetAllCountry()
-//        {
-//            var result = _countryModel.GetAll();
-//            if (result.Count == 0)
-//            {
-//                _countryView.DataEmpty();
-//            }
-//            else
-//            {
-//                _countryView.GetAll(result);
-//            }
-//        }
+        public void GetAllCountry()
+        {
+            var result = _countryModel.GetAll();
+            if (result.Count == 0)
+            {
+                _countryView.DataEmpty();
+            }
+            else
+            {
+                _countryView.GetAll(result);
+            }
+        }
 
-//        public void InsertCountry()
-//        {
-//            var country = _countryView.InsertMenu();
-//            int country = _countryModel.Insert(country);
-//            switch (result)
-//            {
-//                case -1:
-//                    _countryView.Error();
-//                    break;
-//                case 0:
-//                    _countryView.Failure();
-//                    break;
-//                default:
-//                    _countryView.Success();
-//                    break;
-//            }
-//        }
+        public void InsertCountry()
+        {
+            var country = _countryView.InsertMenu();
+            int result = _countryModel.Insert(country);
+            switch (result)
+            {
+                case -1:
+                    _countryView.Error();
+                    break;
+                case 0:
+                    _countryView.Failure();
+                    break;
+                default:
+                    _countryView.Success();
+                    break;
+            }
+        }
 
-//        public void UpdateCountry()
-//        {
-//            var country = _countryView.UpdateMenu();
-//            int result = _countryModel.Update(country);
+        public void UpdateCountry()
+        {
+            var country = _countryView.UpdateMenu();
+            int result = _countryModel.Update(country);
 
-//            switch (result)
-//            {
-//                case -1:
-//                    _countryView.Error();
-//                    break;
-//                case 0:
-//                    _countryView.Failure();
-//                    break;
-//                default:
-//                    _countryView.Success();
-//                    break;
-//            }
-//        }
+            switch (result)
+            {
+                case -1:
+                    _countryView.Error();
+                    break;
+                case 0:
+                    _countryView.Failure();
+                    break;
+                default:
+                    _countryView.Success();
+                    break;
+            }
+        }
 
-//        public void DeleteCountry()
-//        {
-//            var id = _countryView.DeleteMenu();
-//            var result = _countryModel.Delete(id);
+        public void DeleteCountry()
+        {
+            var id = _countryView.DeleteMenu();
+            var result = _countryModel.Delete(id);
 
-//            switch (result)
-//            {
-//                case -1:
-//                    _countryView.Error();
-//                    break;
-//                case 0:
-//                    _countryView.Failure();
-//                    break;
-//                default:
-//                    _countryView.Success();
-//                    break;
-//            }
-//        }
+            switch (result)
+            {
+                case -1:
+                    _countryView.Error();
+                    break;
+                case 0:
+                    _countryView.Failure();
+                    break;
+                default:
+                    _countryView.Success();
+                    break;
+            }
+        }
 
-//        public void SearchCountryById()
-//        {
-//            var id = _countryView.SearchByIdMenu();
-//            var country = _countryModel.GetById(id);
+        public void SearchCountryById()
+        {
+            var id = _countryView.SearchByIdMenu();
+            var country = _countryModel.GetById(id);
 
-//            if (country == null)
-//            {
-//                _countryView.Failure();
-//            }
-//            else
-//            {
-//                _countryView.GetById(country);
-//            }
-//        }
-//    }
-//}
+            if (country == null)
+            {
+                _countryView.Failure();
+            }
+            else
+            {
+                _countryView.GetById(country);
+            }
+        }
+    }
+}
