1d9ed0cfec6690a240fbe20aa2793df8f45f89bf
unknown <ahlulhalliwal09@gmail.com>
2023-07-17 10:56:37 +0700

    Create VLinQ.cs

---
+++ MVCArchitecture/Views/VLinQ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using DatabaseConnectivity.Models;
+
+namespace DatabaseConnectivity.Views
+{
+    public class VLinQ
+    {
+        public void TampilkanEmployeeData(List<LinQ> employees)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"Id: {employee.Id}");
+                Console.WriteLine($"Full Name: {employee.FullName}");
+                Console.WriteLine($"Email: {employee.Email}");
+                Console.WriteLine($"Phone Number: {employee.PhoneNumber}");
+                Console.WriteLine($"Salary: {employee.Salary}");
+                Console.WriteLine($"Department Name: {employee.DepartmentName}");
+                Console.WriteLine($"Street Address: {employee.StreetAddress}");
+                Console.WriteLine($"Country Name: {employee.CountryName}");
+                Console.WriteLine($"Region Name: {employee.RegionName}");
+                Console.WriteLine("==========================");
+            }
+        }
+    }
+}
+
+        
